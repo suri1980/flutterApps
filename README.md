c0f0d24d75d6adf87d1c91ed95f854afede82ee4
suri1980 <suri1980@gmail.com>
2019-03-20 01:02:54 +0800

    Network status

---
+++ README.md
@@ -0,0 +1,11 @@
+# network
+
+A simple Flutter app to show No Internet connection when phone was not on wifi/Cellular
+
+## When device has internet connection
+
+![Screenshot](1.InternetConnection.png)
+
+## When device has no internet connection
+
+![Screenshot](2.NoInternetConnection.png)
---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'connectivity.dart';
+import 'dart:async';
+
+
+//void main() => runApp(MyApp());
+
+void main() {
+   NetworkConnectivity connectionStatus = NetworkConnectivity.getInstance();
+  connectionStatus.initialize();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  StreamSubscription _connectionChangeStream;
+  bool isOffline = false;
+  @override
+  initState() {
+    super.initState();
+    NetworkConnectivity connectionStatus = NetworkConnectivity.getInstance();
+    _connectionChangeStream = connectionStatus.connectionChange.listen(connectionChanged);
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void connectionChanged(dynamic hasConnection) {
+    setState(() {
+      isOffline = !hasConnection;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ( isOffline ) ?  
+    Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body:Center(
+          child: Text("No internet connection"),
+        )
+      ) :  
+    Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            //TestWidget(),
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
